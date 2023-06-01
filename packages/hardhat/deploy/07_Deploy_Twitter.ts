dac97e99050e999203b40bdf4b1d18599f7ddb3d
Guillaume Dieudonné <8122069+GuiDieudo@users.noreply.github.com>
2023-06-01 19:43:32 +0200

    fix: commenting twitter deployment script to allow yarn chain to works

--- packages/hardhat/deploy/07_Deploy_Twitter.ts
+++ packages/hardhat/deploy/07_Deploy_Twitter.ts
@@ -2,8 +2,8 @@ import { ethers } from 'hardhat'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 
-import { giveawayConfig } from '../config/giveawayConfig'
-import { networkConfig } from '../config/networkConfig'
+// import { giveawayConfig } from '../config/giveawayConfig'
+// import { networkConfig } from '../config/networkConfig'
 import { delay } from '../helpers/delay'
 
 const func: DeployFunction = async function ({
@@ -17,158 +17,158 @@ const func: DeployFunction = async function ({
 
   const isLocalDeployment = chainId === '31337' || chainId === '1337'
 
-  const currentGiveawayConfig = giveawayConfig[chainId]
-  if (!currentGiveawayConfig)
-    throw new Error('No giveaway config found for chain id', chainId)
+  // const currentGiveawayConfig = giveawayConfig[chainId]
+  // if (!currentGiveawayConfig)
+  //   throw new Error('No giveaway config found for chain id', chainId)
 
-  let linkTokenAddress: string
-  // TODO load oracle & jobId
-  if (chainId === '31337' || chainId === '1337') {
-    const linkToken = await deployments.get('LinkToken')
-    linkTokenAddress = linkToken.address
-  } else {
-    linkTokenAddress = networkConfig[chainId].linkToken as string
-  }
+  // let linkTokenAddress: string
+  // // TODO load oracle & jobId
+  // if (chainId === '31337' || chainId === '1337') {
+  //   const linkToken = await deployments.get('LinkToken')
+  //   linkTokenAddress = linkToken.address
+  // } else {
+  //   linkTokenAddress = networkConfig[chainId].linkToken as string
+  // }
 
-  if (!linkTokenAddress)
-    throw new Error('Link token address not found for chainId', chainId)
+  // if (!linkTokenAddress)
+  //   throw new Error('Link token address not found for chainId', chainId)
 
-  const deployer = await ethers.getSigner(deployerAddress)
+  // const deployer = await ethers.getSigner(deployerAddress)
 
-  const options = {
-    from: deployerAddress,
-    nonce: 'pending',
-    log: true,
-  }
+  // const options = {
+  //   from: deployerAddress,
+  //   nonce: 'pending',
+  //   log: true,
+  // }
 
-  const {
-    TREASURY_FEE_DEFAULT: treasuryFee,
-    ENCODED_CRON_DEFAULT: encodedCron,
-    API_BASE_URL_DEFAULT: apiBaseUrl,
-  } = currentGiveawayConfig
+  // const {
+  //   TREASURY_FEE_DEFAULT: treasuryFee,
+  //   ENCODED_CRON_DEFAULT: encodedCron,
+  //   API_BASE_URL_DEFAULT: apiBaseUrl,
+  // } = currentGiveawayConfig
 
-  const jobId = ethers.utils.hexlify(
-    ethers.utils.toUtf8Bytes('7223acbd01654282865b678924126013')
-  )
+  // const jobId = ethers.utils.hexlify(
+  //   ethers.utils.toUtf8Bytes('7223acbd01654282865b678924126013')
+  // )
 
-  log('Deploying GiveawayV1 contract')
-  const { address: cronUpkeepAddress } = await deployments.get('CronUpkeep')
-  const { address: cronExternalAddress } = await deployments.get('CronExternal')
-  const cronLibraries = {
-    libraries: {
-      Cron: cronExternalAddress,
-    },
-  }
+  // log('Deploying GiveawayV1 contract')
+  // const { address: cronUpkeepAddress } = await deployments.get('CronUpkeep')
+  // const { address: cronExternalAddress } = await deployments.get('CronExternal')
+  // const cronLibraries = {
+  //   libraries: {
+  //     Cron: cronExternalAddress,
+  //   },
+  // }
 
-  const { address: keeperHelpersAddress } = await deployments.get(
-    'KeeperHelpers'
-  )
-  const { address: tokenHelpersAddress } = await deployments.get('TokenHelpers')
-  const helpersLibraries = {
-    libraries: {
-      KeeperHelpers: keeperHelpersAddress,
-      TokenHelpers: tokenHelpersAddress,
-    },
-  }
+  // const { address: keeperHelpersAddress } = await deployments.get(
+  //   'KeeperHelpers'
+  // )
+  // const { address: tokenHelpersAddress } = await deployments.get('TokenHelpers')
+  // const helpersLibraries = {
+  //   libraries: {
+  //     KeeperHelpers: keeperHelpersAddress,
+  //     TokenHelpers: tokenHelpersAddress,
+  //   },
+  // }
 
-  // const keeperArgs = [
-  //   cronUpkeepAddress,
-  //   'refreshActiveGiveawayStatus()',
-  //   encodedCron,
+  // // const keeperArgs = [
+  // //   cronUpkeepAddress,
+  // //   'refreshActiveGiveawayStatus()',
+  // //   encodedCron,
+  // // ]
+
+  // // const {
+  // //   address: keeperAddress,
+  // //   newlyDeployed: keeperNewlyDeployed,
+  // //   receipt: { gasUsed: keeperGasUsed },
+  // // } = await deploy('Keeper', {
+  // //   ...options,
+  // //   ...cronLibraries,
+  // //   args: keeperArgs,
+  // // })
+
+  // // if (keeperNewlyDeployed)
+  // //   log(
+  // //     `✅ Contract Keeper deployed at ${keeperAddress} using ${keeperGasUsed} gas`
+  // //   )
+
+  // // log('Adding Keeper to Keeper delegators')
+  // // cronUpkeep.addDelegator(keeperAddress)
+
+  // // TODO Load it from config
+  // const giveawayArgs = [
+  //   jobId,
+  //   apiBaseUrl,
+  //   '0xCC79157eb46F5624204f47AB42b3906cAA40eaB7',
+  //   linkTokenAddress,
+  //   treasuryFee,
+  //   // cronUpkeepAddress,
+  //   // keeperAddress,
+  //   // encodedCron,
+  //   [],
+  //   [],
   // ]
 
   // const {
-  //   address: keeperAddress,
-  //   newlyDeployed: keeperNewlyDeployed,
-  //   receipt: { gasUsed: keeperGasUsed },
-  // } = await deploy('Keeper', {
+  //   address: giveawayAddress,
+  //   newlyDeployed: giveawayNewlyDeployed,
+  //   receipt: { gasUsed: giveawayGasUsed },
+  // } = await deploy('GiveawayV1', {
   //   ...options,
-  //   ...cronLibraries,
-  //   args: keeperArgs,
+  //   ...helpersLibraries,
+  //   args: giveawayArgs,
   // })
 
-  // if (keeperNewlyDeployed)
+  // if (giveawayNewlyDeployed)
   //   log(
-  //     `✅ Contract Keeper deployed at ${keeperAddress} using ${keeperGasUsed} gas`
+  //     `✅ Contract GiveawayV1 deployed at ${giveawayAddress} using ${giveawayGasUsed} gas`
   //   )
 
-  // log('Adding Keeper to Keeper delegators')
-  // cronUpkeep.addDelegator(keeperAddress)
+  // log('Adding GiveawayV1 to Keeper delegators')
+  // try {
+  //   const { interface: cronUpkeepInterface } = await ethers.getContractFactory(
+  //     'CronUpkeep',
+  //     cronLibraries
+  //   )
 
-  // TODO Load it from config
-  const giveawayArgs = [
-    jobId,
-    apiBaseUrl,
-    '0xCC79157eb46F5624204f47AB42b3906cAA40eaB7',
-    linkTokenAddress,
-    treasuryFee,
-    // cronUpkeepAddress,
-    // keeperAddress,
-    // encodedCron,
-    [],
-    [],
-  ]
+  //   const cronUpkeep = new ethers.Contract(
+  //     cronUpkeepAddress,
+  //     cronUpkeepInterface,
+  //     deployer
+  //   )
+  //   cronUpkeep.addDelegator(giveawayAddress)
+  // } catch (error) {
+  //   log(
+  //     '[ERROR] When adding Keeper to Keeper delegators, trying without Cron library'
+  //   )
 
-  const {
-    address: giveawayAddress,
-    newlyDeployed: giveawayNewlyDeployed,
-    receipt: { gasUsed: giveawayGasUsed },
-  } = await deploy('GiveawayV1', {
-    ...options,
-    ...helpersLibraries,
-    args: giveawayArgs,
-  })
+  //   const { interface: cronUpkeepInterface } = await ethers.getContractFactory(
+  //     'CronUpkeep'
+  //   )
 
-  if (giveawayNewlyDeployed)
-    log(
-      `✅ Contract GiveawayV1 deployed at ${giveawayAddress} using ${giveawayGasUsed} gas`
-    )
+  //   const cronUpkeep = new ethers.Contract(
+  //     cronUpkeepAddress,
+  //     cronUpkeepInterface,
+  //     deployer
+  //   )
+  //   cronUpkeep.addDelegator(giveawayAddress)
+  // }
 
-  log('Adding GiveawayV1 to Keeper delegators')
-  try {
-    const { interface: cronUpkeepInterface } = await ethers.getContractFactory(
-      'CronUpkeep',
-      cronLibraries
-    )
+  // if (isLocalDeployment || !giveawayNewlyDeployed) return
 
-    const cronUpkeep = new ethers.Contract(
-      cronUpkeepAddress,
-      cronUpkeepInterface,
-      deployer
-    )
-    cronUpkeep.addDelegator(giveawayAddress)
-  } catch (error) {
-    log(
-      '[ERROR] When adding Keeper to Keeper delegators, trying without Cron library'
-    )
-
-    const { interface: cronUpkeepInterface } = await ethers.getContractFactory(
-      'CronUpkeep'
-    )
-
-    const cronUpkeep = new ethers.Contract(
-      cronUpkeepAddress,
-      cronUpkeepInterface,
-      deployer
-    )
-    cronUpkeep.addDelegator(giveawayAddress)
-  }
-
-  if (isLocalDeployment || !giveawayNewlyDeployed) return
-
-  log(`🕦 Waiting before verification...`)
-  await delay(30 * 1000)
-  try {
-    log(`✅ Verifying contract GiveawayV1`)
-    await hre.run('verify:verify', {
-      address: giveawayAddress,
-      constructorArguments: giveawayArgs,
-    })
-    log(`🕧 Waiting post verification...`)
-    await delay(10 * 1000)
-  } catch (error) {
-    console.error('Error during contract verification', error.message)
-  }
+  // log(`🕦 Waiting before verification...`)
+  // await delay(30 * 1000)
+  // try {
+  //   log(`✅ Verifying contract GiveawayV1`)
+  //   await hre.run('verify:verify', {
+  //     address: giveawayAddress,
+  //     constructorArguments: giveawayArgs,
+  //   })
+  //   log(`🕧 Waiting post verification...`)
+  //   await delay(10 * 1000)
+  // } catch (error) {
+  //   console.error('Error during contract verification', error.message)
+  // }
 }
 
 func.tags = ['all', 'test', 'dev', 'staging', 'prod', 'giveaway']
