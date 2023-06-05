da3388da9efe2dcdd0509689c096226075b76f74
Axel <mtn.axel@gmail.com>
2023-06-05 16:35:10 +0200

    Refined the validation process

--- packages/hardhat/contracts/connectors/Twitter.sol
+++ packages/hardhat/contracts/connectors/Twitter.sol
@@ -24,18 +24,18 @@ ValidateCampaignClaim:
 */
 
 contract TwitterV1 is Pausable, Ownable, ReentrancyGuard, ChainlinkClient {
-    using Chainlink for Chainlink.Request;
-    using EnumerableMap for EnumerableMap.UintToAddressMap;
-    using Counters for Counters.Counter;
+  using Chainlink for Chainlink.Request;
+  using EnumerableMap for EnumerableMap.UintToAddressMap;
+  using Counters for Counters.Counter;
 
-    address public keeper;
+  address public keeper;
 
-    bytes32 public immutable jobId;
-    uint256 private immutable fee;
+  bytes32 public immutable jobId;
+  uint256 private immutable fee;
 
-    string public requestBaseURI;
+  string public requestBaseURI;
 
-    EnumerableMap.UintToAddressMap private users;
+  EnumerableMap.UintToAddressMap private users;
 
   uint256 lastCampaignId;
 
@@ -58,52 +58,52 @@ contract TwitterV1 is Pausable, Ownable, ReentrancyGuard, ChainlinkClient {
   mapping(uint256 => Campaign) campaignById;
   mapping(uint256 => CampaignContributors) campaignContributorsByCampaignId;
 
-    uint256 public constant MAX_TREASURY_FEE = 1000; // 10%
+  uint256 public constant MAX_TREASURY_FEE = 1000; // 10%
 
-    uint256 public treasuryFee; // treasury rate (e.g. 200 = 2%, 150 = 1.50%)
-    uint256 public treasuryAmount; // treasury amount that was not claimed
+  uint256 public treasuryFee; // treasury rate (e.g. 200 = 2%, 150 = 1.50%)
+  uint256 public treasuryAmount; // treasury amount that was not claimed
 
-    ///
-    /// EVENTS
-    ///
-    /**
-     * @notice Called when a request to sign up is made
-     */
-    event SignUpRequested(uint256 indexed userId, bytes32 indexed requestId);
-    /**
-     * @notice Called when a user signed up
-     */
-    event SignedUp(uint256 indexed userId, address userAddress);
+  ///
+  /// EVENTS
+  ///
+  /**
+   * @notice Called when a request to sign up is made
+   */
+  event SignUpRequested(uint256 indexed userId, bytes32 indexed requestId);
+  /**
+   * @notice Called when a user signed up
+   */
+  event SignedUp(uint256 indexed userId, address userAddress);
 
   event CampaignCreated(uint256 campaignId, address company);
   event CampaignClosed(uint256 campaignId);
   event ContributorAdded(uint256 campaignId, address contributor);
   event ContributorRetributed(uint256 campaignId, address contributor, uint256 retribution);
 
-    ///
-    /// CONSTRUCTOR
-    ///
+  ///
+  /// CONSTRUCTOR
+  ///
 
-    /**
-     * @notice Initialize the link token and target oracle
-     * All testnets config : https://docs.chain.link/any-api/testnet-oracles/
-     */
-    constructor(
-      bytes32 _jobId,
-      string memory _requestBaseURI,
-      address _oracle,
-      address _link,
-      uint256 _treasuryFee
-    ) {
-      setChainlinkToken(_link);
-      setChainlinkOracle(_oracle);
-      jobId = _jobId;
-      // 0,1 * 10**18 (Varies by network and job)
-      fee = (1 * LINK_DIVISIBILITY) / 10;
+  /**
+   * @notice Initialize the link token and target oracle
+   * All testnets config : https://docs.chain.link/any-api/testnet-oracles/
+   */
+  constructor(
+    bytes32 _jobId,
+    string memory _requestBaseURI,
+    address _oracle,
+    address _link,
+    uint256 _treasuryFee
+  ) {
+    setChainlinkToken(_link);
+    setChainlinkOracle(_oracle);
+    jobId = _jobId;
+    // 0,1 * 10**18 (Varies by network and job)
+    fee = (1 * LINK_DIVISIBILITY) / 10;
 
-      treasuryFee = _treasuryFee;
-      requestBaseURI = _requestBaseURI;
-    }
+    treasuryFee = _treasuryFee;
+    requestBaseURI = _requestBaseURI;
+  }
 
   ///
   /// MAIN FUNCTIONS
@@ -145,8 +145,10 @@ contract TwitterV1 is Pausable, Ownable, ReentrancyGuard, ChainlinkClient {
 
   function claimCampaignContribution(uint256 campaignId) external {
     // TODO:
-    // - Use the internal _validate function to verify the msg.sender has indeed reached the validationThreshold etc
-
+    // Use the internal _validateContributionClaim function to verify the msg.sender has indeed reached the validationThreshold etc
+    // For now this internal function _validateContributionClaim always return true
+    bool isClaimValid = _validateContributionClaim();
+    require(isClaimValid == true, "This contribution does not satisfy the campaign's requirements");
     _addPartakerToCampaignContributors(campaignId, msg.sender);
     emit ContributorAdded(campaignId, msg.sender);
   }
@@ -164,143 +166,143 @@ contract TwitterV1 is Pausable, Ownable, ReentrancyGuard, ChainlinkClient {
     emit ContributorRetributed(campaignId, msg.sender, retributionAmount);
   }
 
-    /**
-     * @notice Function that allow user to sign up to twitter and claim won twitters
-     */
-    function signUp(uint256 _userId) external {
-      users.set(_userId, msg.sender);
+  /**
+   * @notice Function that allow user to sign up to twitter and claim won twitters
+   */
+  function signUp(uint256 _userId) external {
+    users.set(_userId, msg.sender);
 
-      emit SignedUp(_userId, msg.sender);
-      _requestSignUp(_userId);
-    }
+    emit SignedUp(_userId, msg.sender);
+    _requestSignUp(_userId);
+  }
 
-    /**
-     * @notice Check if user has signed up
-     * @param _userId - twitter user id
-     */
-    function hasSignedUp(uint256 _userId) external view returns (bool) {
-      return users.contains(_userId);
-    }
+  /**
+   * @notice Check if user has signed up
+   * @param _userId - twitter user id
+   */
+  function hasSignedUp(uint256 _userId) external view returns (bool) {
+    return users.contains(_userId);
+  }
 
-    /**
-     * @notice Check if caller has signed up
-     */
-    function hasSignedUp() external view returns (bool) {
-      bool found = false;
-      for (uint256 idx = 0; idx < users.length(); idx++) if (users.get(idx) == msg.sender) found = true;
-      return found;
-    }
+  /**
+   * @notice Check if caller has signed up
+   */
+  function hasSignedUp() external view returns (bool) {
+    bool found = false;
+    for (uint256 idx = 0; idx < users.length(); idx++) if (users.get(idx) == msg.sender) found = true;
+    return found;
+  }
 
-    ///
-    /// CHAINLINK RESPONSES
-    ///
+  ///
+  /// CHAINLINK RESPONSES
+  ///
 
-    /**
-     * @notice Fulfill Sign Up request
-     *
-     * @param _requestId - id of the request
-     * @param _userId - twitter user id
-     * @param _hasSignedUp - true if  twitter userId match msg.sender address
-     */
-    function fulfillSignUp(
-      bytes32 _requestId,
-      uint256 _userId,
-      bool _hasSignedUp
-    ) public recordChainlinkFulfillment(_requestId) {
-      if (!_hasSignedUp) users.remove(_userId);
+  /**
+   * @notice Fulfill Sign Up request
+   *
+   * @param _requestId - id of the request
+   * @param _userId - twitter user id
+   * @param _hasSignedUp - true if  twitter userId match msg.sender address
+   */
+  function fulfillSignUp(
+    bytes32 _requestId,
+    uint256 _userId,
+    bool _hasSignedUp
+  ) public recordChainlinkFulfillment(_requestId) {
+    if (!_hasSignedUp) users.remove(_userId);
 
-      address user = users.get(_userId);
-      // TODO
-      // for (uint256 round = 0; round < epoch.current(); round++)
-      //   for (uint256 idx = 0; idx < winners[round].length; idx++)
-      //     if (winners[round][idx].userId == _userId) winners[round][idx].playerAddress = user;
-    }
+    address user = users.get(_userId);
+    // TODO
+    // for (uint256 round = 0; round < epoch.current(); round++)
+    //   for (uint256 idx = 0; idx < winners[round].length; idx++)
+    //     if (winners[round][idx].userId == _userId) winners[round][idx].playerAddress = user;
+  }
 
-    ///
-    /// GETTERS FUNCTIONS
-    ///
+  ///
+  /// GETTERS FUNCTIONS
+  ///
 
-    /**
-     * @notice Return URI for twitter endpoint
-     */
-    function getTwitterURI(uint256 _twitterId) public view returns (string memory _twitterURI) {
-      return
-        string(
-          abi.encodePacked(
-            requestBaseURI,
-            "/chains/",
-            block.chainid,
-            "/twitters/",
-            _twitterId,
-            "/winners"
-            // TODO: Add query params
-            //   "?prizes=",
-            //   prizes[_twitterId].length,
-            //   "&tweetId",
-            //   twitters[_twitterId].tweetId,
-            //   "&retweetMaxCount",
-            //   twitters[_twitterId].retweetMaxCount
-          )
-        );
-    }
+  /**
+   * @notice Return URI for twitter endpoint
+   */
+  function getTwitterURI(uint256 _twitterId) public view returns (string memory _twitterURI) {
+    return
+      string(
+        abi.encodePacked(
+          requestBaseURI,
+          "/chains/",
+          block.chainid,
+          "/twitters/",
+          _twitterId,
+          "/winners"
+          // TODO: Add query params
+          //   "?prizes=",
+          //   prizes[_twitterId].length,
+          //   "&tweetId",
+          //   twitters[_twitterId].tweetId,
+          //   "&retweetMaxCount",
+          //   twitters[_twitterId].retweetMaxCount
+        )
+      );
+  }
 
   function getCampaign(uint256 campaignId) external view returns (Campaign memory) {
     return _getCampaign(campaignId);
   }
 
-    /**
-     * @notice Return URI for sign up endpoint
-     */
-    function getSignUpURI(uint256 _userId) public view returns (string memory _signUpURI) {
-      return string(abi.encodePacked(requestBaseURI, "/users/", _userId));
-    }
+  /**
+   * @notice Return URI for sign up endpoint
+   */
+  function getSignUpURI(uint256 _userId) public view returns (string memory _signUpURI) {
+    return string(abi.encodePacked(requestBaseURI, "/users/", _userId));
+  }
 
-    ///
-    /// ADMIN FUNCTIONS
-    ///
+  ///
+  /// ADMIN FUNCTIONS
+  ///
 
-    /**
-     * @notice Witdraws LINK from the contract to the Owner
-     * @dev only admin can call this function
-     */
-    function withdrawLink() external onlyAdmin {
-      LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
-      require(link.transfer(owner(), link.balanceOf(address(this))), "Unable to transfer");
-    }
+  /**
+   * @notice Witdraws LINK from the contract to the Owner
+   * @dev only admin can call this function
+   */
+  function withdrawLink() external onlyAdmin {
+    LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+    require(link.transfer(owner(), link.balanceOf(address(this))), "Unable to transfer");
+  }
 
-    /**
-     * @notice Set requestBaseURI
-     * @dev only admin can call this function
-     */
-    function setRequestBaseURI(string calldata _requestBaseURI) external onlyAdmin {
-      requestBaseURI = _requestBaseURI;
-    }
+  /**
+   * @notice Set requestBaseURI
+   * @dev only admin can call this function
+   */
+  function setRequestBaseURI(string calldata _requestBaseURI) external onlyAdmin {
+    requestBaseURI = _requestBaseURI;
+  }
 
-    /**
-     * @notice Add user to users list
-     * @dev only admin can call this function
-     */
-    function addUser(uint256 _userId, address _userAddress) external onlyAdmin {
-      if (!users.contains(_userId)) users.set(_userId, _userAddress);
-    }
+  /**
+   * @notice Add user to users list
+   * @dev only admin can call this function
+   */
+  function addUser(uint256 _userId, address _userAddress) external onlyAdmin {
+    if (!users.contains(_userId)) users.set(_userId, _userAddress);
+  }
 
-    /**
-     * @notice Call this method if no response is received within 5 minutes
-     * @param _requestId The ID that was generated for the request to cancel
-     * @param _payment The payment specified for the request to cancel
-     * @param _callbackFunctionId The bytes4 callback function ID specified for
-     * the request to cancel
-     * @param _expiration The expiration generated for the request to cancel
-     * @dev only admin can call this function
-     */
-    function cancelRequest(
-      bytes32 _requestId,
-      uint256 _payment,
-      bytes4 _callbackFunctionId,
-      uint256 _expiration
-    ) public onlyAdmin {
-      cancelChainlinkRequest(_requestId, _payment, _callbackFunctionId, _expiration);
-    }
+  /**
+   * @notice Call this method if no response is received within 5 minutes
+   * @param _requestId The ID that was generated for the request to cancel
+   * @param _payment The payment specified for the request to cancel
+   * @param _callbackFunctionId The bytes4 callback function ID specified for
+   * the request to cancel
+   * @param _expiration The expiration generated for the request to cancel
+   * @dev only admin can call this function
+   */
+  function cancelRequest(
+    bytes32 _requestId,
+    uint256 _payment,
+    bytes4 _callbackFunctionId,
+    uint256 _expiration
+  ) public onlyAdmin {
+    cancelChainlinkRequest(_requestId, _payment, _callbackFunctionId, _expiration);
+  }
 
   ///
   /// INTERNAL FUNCTIONS
@@ -318,10 +320,11 @@ contract TwitterV1 is Pausable, Ownable, ReentrancyGuard, ChainlinkClient {
    * @notice validates the input to performUpkeep
    * @param _twitterId the id of the cron job
    */
-  function _validate(uint256 _twitterId) private view {
+  function _validateContributionClaim(uint256 _twitterId) private view {
     // TODO implement and use
     // require(winners[_twitterId].length == 0, "Twitter winners already requesteds");
     // require(!twitters[_twitterId].isEnded, "Twitter already ended");
+    return true;
   }
 
   function _addPartakerToCampaignContributors(uint256 campaignId, address partaker) internal {
@@ -344,29 +347,29 @@ contract TwitterV1 is Pausable, Ownable, ReentrancyGuard, ChainlinkClient {
     return retributionAmount;
   }
 
-    /**
-     * @notice Create request to sign up user
-     * @dev only admin can call this function
-     */
-    function _requestSignUp(uint256 _userId) private onlyAdmin whenNotPaused returns (bytes32 _requestId) {
-      Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfillSignUp.selector);
-      req.add("get", getSignUpURI(_userId));
-      // https://docs.chain.link/any-api/testnet-oracles/
-      req.add("path", "hasSignedUp");
-      bytes32 requestId = sendChainlinkRequest(req, fee);
-      emit SignUpRequested(_userId, requestId);
-      return requestId;
-    }
+  /**
+   * @notice Create request to sign up user
+   * @dev only admin can call this function
+   */
+  function _requestSignUp(uint256 _userId) private onlyAdmin whenNotPaused returns (bytes32 _requestId) {
+    Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfillSignUp.selector);
+    req.add("get", getSignUpURI(_userId));
+    // https://docs.chain.link/any-api/testnet-oracles/
+    req.add("path", "hasSignedUp");
+    bytes32 requestId = sendChainlinkRequest(req, fee);
+    emit SignUpRequested(_userId, requestId);
+    return requestId;
+  }
 
-  /
-  / MODIFIERS
-  /
+  ///
+  /// MODIFIERS
+  ///
 
-    /**
-     * @notice Modifier that ensure only admin can access this function
-     */
-    modifier onlyAdmin() {
-      require(msg.sender == owner(), "Caller is not the admin");
-      _;
-    }
+  /**
+   * @notice Modifier that ensure only admin can access this function
+   */
+  modifier onlyAdmin() {
+    require(msg.sender == owner(), "Caller is not the admin");
+    _;
+  }
 }
