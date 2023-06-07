2154cf1181a3cb283c20f5c83c8ec29bf44753cd
hsjo12 <hsjo12@naver.com>
2023-06-08 01:10:03 +0900

    change counter router type

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -60,7 +60,7 @@ router.get("/tweets/:tweetId/retweetCounter", async ({ params }) => {
 
 	try {
 		const result = await getAllRetweets(tweetId);
-		return json({ counter: String(result.length) });
+		return json({ counter: Number(result.length) });
 	} catch (err) {
 		return error(500, { error: { message: err.message } });
 	}
